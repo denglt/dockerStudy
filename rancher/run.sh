14199c159ad8ed7a5e3bb9b699b32314a77b19a3
denglt <ltdeng@hotmail.com>
2022-01-14 09:55:39 +0800

    rancher

--- rancher/run.sh
+++ rancher/run.sh
@@ -1,4 +1,6 @@
-docker run --privileged -d --restart=unless-stopped \
+docker stop rancher
+docker rm rancher
+docker run --name rancher --privileged -d --restart=unless-stopped \
 	-p 80:80 -p 443:443 \
        	-v /root/docker/rancher:/var/lib/rancher/ \
 	rancher/rancher:stable
